e60bc7752f086881340536d5ded7a97f525a4994
M42 <109526619+42mgr@users.noreply.github.com>
2025-05-20 16:19:00 +0200

    Update BuildAndDeploy.sh

--- build/BuildAndDeploy.sh
+++ build/BuildAndDeploy.sh
@@ -3,5 +3,5 @@ export MONO_IOMAP=all
 export VSToolsPath=msbuild
 export DeployTo=$1
 
-msbuild msbuild/build.proj /flp:LogFile=Build.log
-msbuild msbuild/deploy.proj /flp:LogFile=Deploy.log
+msbuild build/msbuild/build.proj /flp:LogFile=Build.log
+msbuild build/msbuild/deploy.proj /flp:LogFile=Deploy.log
